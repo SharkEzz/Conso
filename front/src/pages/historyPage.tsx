aef1edec05f7a65a3f656f59e13b6b1683fe4a95
SharkEzz <icraft640@gmail.com>
2022-04-10 21:26:14 +0200

    fix: fix date selector

--- front/src/pages/historyPage.tsx
+++ front/src/pages/historyPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Input, Spinner } from '@chakra-ui/react';
+import { Box, Flex, Input, Spinner, Text } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import ReactDatePicker from 'react-datepicker';
 import { useQuery } from 'react-query';
@@ -9,9 +9,7 @@ const currentDate = new Date();
 
 export default function HistoryPage() {
   const [startDate, setStartDate] = useState<Date | null>(currentDate);
-  const [stopDate, setStopDate] = useState<Date | null>(
-    new Date(new Date().setDate(currentDate.getDate() + 1)),
-  );
+  const [stopDate, setStopDate] = useState<Date | null>(currentDate);
   const { data, isLoading, isError, refetch } = useQuery(
     'historyStats',
     async () => fetchHistoryStats(startDate, stopDate),
@@ -32,22 +30,25 @@ export default function HistoryPage() {
   return (
     <>
       <Box mb={6}>
-        <Box w="xs" ml="auto">
-          <ReactDatePicker
-            selected={startDate}
-            onChange={(dates) => {
-              const [start, end] = dates;
-              setStartDate(start);
-              setStopDate(end);
-            }}
-            customInput={<Input textAlign="center" />}
-            startDate={startDate}
-            endDate={stopDate}
-            maxDate={new Date()}
-            selectsRange
-            dateFormat="dd/MM/yyyy"
-          />
-        </Box>
+        <Flex justify="flex-end" align="center" gap={3}>
+          <Text>Filtrer par dates : </Text>
+          <Box>
+            <ReactDatePicker
+              selected={startDate}
+              onChange={(dates) => {
+                const [start, end] = dates;
+                setStartDate(start);
+                setStopDate(end);
+              }}
+              customInput={<Input textAlign="center" cursor="pointer" />}
+              startDate={startDate}
+              endDate={stopDate}
+              maxDate={new Date()}
+              selectsRange
+              dateFormat="dd/MM/yyyy"
+            />
+          </Box>
+        </Flex>
       </Box>
       {Object.keys(data).map((day) => (
         <TotalDay key={day} stats={data[day as any]} />
